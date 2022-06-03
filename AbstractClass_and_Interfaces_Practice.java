daf031fbea712b021b789022e8164fbea59f94a1
Krunal Gupta <guptakrunal02@gmail.com>
2022-06-03 17:15:00 +0530

    Abstract Class and Interface Practice in Java

--- AbstractClass_and_Interfaces_Practice.java
+++ AbstractClass_and_Interfaces_Practice.java
@@ -15,11 +15,50 @@ class FountainPen extends Pen{
         System.out.println("Changing the Nib");
     }
 }
+class Monkey{
+    void jump(){
+        System.out.println("Jumping...");
+    }
+    void bite(){
+        System.out.println("Biting...");
+    }
+}
+interface BasicAnimal{
+    void eat();
+    void sleep();
+}
+class Human extends Monkey implements BasicAnimal{
+    void speak(){
+        System.out.println("Hello Sir!");
+    }
+    @Override
+    public void sleep(){
+        System.out.println("Sleeping...");
+    }
+    public void eat(){
+        System.out.println("Eating...");
+    }
+}
 public class AbstractClass_and_Interfaces_Practice {
     public static void main(String[] args) {
         FountainPen pen = new FountainPen();
         pen.changeNib();
-        
+
+        Human Krg = new Human();
+        Krg.sleep();
+
+        Monkey m1 = new Human();
+//        m1.speak();       // Cannot use speak method bcz the reference is monkey which does not have speak method
+        m1.jump();
+        m1.bite();
+
+        BasicAnimal ba = new Human();
+//        ba.speak();           // Error
+        ba.eat();
+        ba.sleep();
+
+
+
 
     }
 }
